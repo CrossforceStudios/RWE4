66ae8daac55a8a5a75536e710f1df4b05568d7b2
Aerodos12 <30639238+Aerodos12@users.noreply.github.com>
2024-09-25 10:09:49 -0400

    ServerSettings + Event System Install

--- src/EngineInstall/ServerScriptService/Main.server.lua
+++ src/EngineInstall/ServerScriptService/Main.server.lua
@@ -8,3 +8,9 @@ Resources:SetupFlags({
 -- Necessary Modules
 local Players = game:GetService("Players")
 local RemoteService = Resources:LoadLibrary("RemoteService")
+local EventSystem = Resources:LoadLibrary("EventUtils")
+-- Event System
+local ServerSettings = require(script.Parent.ServerSettings)
+for _, event in ServerSettings.Events do
+    EventSystem:AddEvent(event)
+end
---
+++ src/EngineInstall/ServerScriptService/ServerSettings.lua
@@ -0,0 +1,7 @@
+return {
+
+    Events = {
+        "KillAdded";
+        "PlayerAdded";
+    }
+};
