01b2ff903cad136019fe6b15577158a337afe3b8
Aerodos12 <30639238+Aerodos12@users.noreply.github.com>
2024-09-26 13:59:08 -0400

    Interaction Manager + Templates

---
+++ src/Modules/InteractionManager/init.lua
@@ -0,0 +1,28 @@
+return function(RWE4,Plugin,EditorLibraries,Libraries,loader,RBLXGUI,EI)
+	local imUI = RBLXGUI.PluginWidget.new({
+		ID = "InteractionManager_RWE4";
+		Enabled = false;
+		Title = "RWE4 Interaction Manager";
+		DockState = Enum.InitialDockState.Float;
+	}, dwi)
+	local pageCreate = RBLXGUI.Page.new({
+		Name = "CREATE/EDIT";
+		Open = true;
+	}, installerUI)
+	local pageManager = RBLXGUI.Page.new({
+		Name = "MANAGE";
+		Open = false;
+	}, installerUI)
+
+    local function openIntManager(selection)
+		imUI.Content.Enabled = true
+    end
+
+    return {
+		Title = "Interaction Manager";
+		Icon = "rbxassetid://5568203747";
+		OnSelect = function()
+			openIntManager(game.Selection:Get()[1])
+		end;
+	}
+end
---
+++ src/Templates/InteractionTemplate.lua
@@ -0,0 +1,23 @@
+return [[
+    return {
+        Name = "%s";
+        Context = "Interaction";
+        Description = "%s";
+        KeyContext = "Regular";
+        ActionType = "%s";
+        ActionBuilder = function(object)
+            return ("Do"):upper()
+        end;
+        CreationCondition = function(object)
+            return object.Name == "Example";
+        end;
+        ObjectBuilder = function(object)
+            return ("This"):upper()
+        end,
+        PlayerCondition = function(player,object)
+            return true
+        end;
+        Location = function() return workspace.CurrentMap.Value:FindFirstChild("Example",true); end;
+        ActivationDistance = 15;
+    }
+]]
